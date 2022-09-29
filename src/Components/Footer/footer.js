b915780f609a16e29e11f2a908d984069145bb7c
Trikaloski <nicolasmosca9999@gmail.com>
2022-09-28 21:18:02 -0300

    footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,7 +1,18 @@
+import React from 'react';
 render() ;{
     return (
       <>
-
+        <html lang="es">
+        <head>
+          <meta charset="UTF-8">
+          <title>Pie de Pagina</title>
+          <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+          <link rel="stylesheet" href="footer.css">
+        </head>
+        <body>
+          <footer class="pie-pagina"></footer>
+        </body>
+        </html>
       </>
     );
   }
