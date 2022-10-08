32a19ac8edff639aa61841937fd592d4ac5ec017
Trikaloski <nicolasmosca9999@gmail.com>
2022-10-08 15:36:27 -0300

    bodycards

--- src/Components/Body/body.js
+++ src/Components/Body/body.js
@@ -7,17 +7,39 @@ class Body extends React.Component {
         return (
             <>
                 <body>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Comprar</Button>
-                        </Card.Body>
-                    </Card>
+                    <header class="bodydefault">
+                        <h2>Productos</h2>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./img/Juguetes.png" />
+                            <Card.Body>
+                                <Card.Title>Juguetes</Card.Title>
+                                <Card.Text>
+                                    Todo tipo de juguetes...
+                                </Card.Text>
+                                <Button variant="primary">Comprar</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./img/Decoración.png" />
+                            <Card.Body>
+                                <Card.Title>Decoración</Card.Title>
+                                <Card.Text>
+                                    Artículos decorativos, souvenires, carteles...
+                                </Card.Text>
+                                <Button variant="primary">Comprar</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./img/Personalizados.png" />
+                            <Card.Body>
+                                <Card.Title>Personalizados</Card.Title>
+                                <Card.Text>
+                                    Mostranos tu idea y nosotros la produciremos...
+                                </Card.Text>
+                                <Button variant="primary">Comprar</Button>
+                            </Card.Body>
+                        </Card>
+                    </header>
                 </body>
             </>
         )
